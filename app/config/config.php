8cbc7ef91b30edb512be629d26d6e71aa10a713c
Patryk <66371791@wp.pl>
2019-03-10 12:32:47 +0100

    add in config constants

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php 
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', 'http://localhost:8080/mvc');
+    // Site Name
+    define('SITENAME', 'MVC');
